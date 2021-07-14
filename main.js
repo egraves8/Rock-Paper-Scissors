a36ac122608b326e25ab2fb0e8973519ab5b8e18
egraves8 <techbygene@gmail.com>
2021-07-14 13:27:03 -0400

    ui-version1

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+let yourScore = 0;
+let comScore = 0;    
+let result;
+
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let choice = btn.id;
+        console.log(choice);
+        result = playRound(choice, computerPlay());
+        console.log(result);
+        if (result == "W") {
+            yourScore = incrementScore(yourScore);
+        }
+        if (result == "L") {
+            comScore = incrementScore(comScore);
+        } 
+        /*if (result == "WL") {
+            yourScore = incrementScore(yourScore);
+            comScore = incrementScore(comScore);
+        }*/
+        console.log(String(yourScore + " - " + comScore));
+        scoreboard.innerText = String(yourScore) + " - " + String(comScore);
+    })
+})
+
+const scoreboard = document.createElement('center');
+scoreboard.innerText = String(yourScore) + " - " + String(comScore);
+scoreboard.style.color = '#03DAC5';
+scoreboard.style.fontSize = "4em";
+document.body.appendChild(scoreboard);
+
+const report = document.createElement('center');
+report.style.color = "white";
+document.body.appendChild(report);
+
+
 function computerPlay(){
     let x = Math.ceil(Math.random() * 3);
     var choice;
@@ -14,46 +51,66 @@ function incrementScore(number){
     return number + 1;
 }
 function playRound(playerSelection, computerSelection){
-    var pS = prompt("Choose (Rock, Paper, Scissors)");
-    playerSelection = pS;
+    //var pS = prompt("Choose (Rock, Paper, Scissors)");
     console.log("You Chose: " + playerSelection);
     console.log("The Computer: " + computerSelection);
-    if(playerSelection == "Rock") {
+    if(playerSelection == "rock") {
         if (computerSelection == "Rock") {
             console.log("Tie!");
+            report.innerText = "Tie, you both chose Rock!";
             return "WL";
         } else if (computerSelection == "Paper") {
             console.log("You Lose! Paper beats Rock.");
+            report.innerText = "You Lose! Paper beats Rock!";
             return "L";
         } else {
             console.log("You Win! Rock beats Scissors.");
+            report.innerText = "You Win! Rock beats Scissors!";
             return "W";
         }
-    } else if (playerSelection == "Paper") {
+    } else if (playerSelection == "paper") {
         if (computerSelection == "Rock") {
             console.log("You Win! Paper beats Rock.");
+            report.innerText = "You Win! Paper beats Rock!";
             return "W";
         } else if (computerSelection == "Paper") {
             console.log("Tie!");
+            report.innerText = "Tie, you both chose Paper!";
             return "WL";
         } else {
             console.log("You Lose! Scissors beats Paper.");
+            report.innerText = "You Lose! Scissors beats Paper!";
             return "L";
         }
     } else {
         if (computerSelection == "Rock") {
             console.log("You Lose! Rock beats Scissors.");
+            report.innerText = "You Lose! Rock beats Scissors!";
             return "L";
         } else if (computerSelection == "Paper") {
             console.log("You Win! Scissors beats Paper.");
+            report.innerText = "You Win! Scissors beats Paper!";
             return "W";
         } else {
             console.log("Tie!");
+            report.innerText = "Tie, you both chose Scissors!";
             return "WL";
         }
     }
 }
-function game(){
+
+function scoreForRound(yourScore, comScore){
+    if (result == "W") {
+        yourScore = incrementScore(yourScore);
+    } else if (result == "L") {
+        comScore = incrementScore(comScore);
+    } else {
+        yourScore = incrementScore(yourScore);
+        comScore = incrementScore(comScore);
+    }
+    console.log(String(yourScore + " - " + comScore));
+}
+/*function game(){
     let yourScore = 0;
     let comScore = 0;
     let result;
@@ -78,6 +135,6 @@ function game(){
         console.log("Tie!");
         console.log("Score: " + String(yourScore) + " - " + String(comScore));
     }
-}
+}*/
 
-game();
+//game();
